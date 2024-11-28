d25fb039e661cffd6a12df5bd8ac7839487de960
Kordian Wojnowski <wojnowski.kordian2002@gmail.com>
2024-11-28 16:31:48 +0100

    Dodano paczkę hotel

---
+++ src/programowanieOb/figury/Glowna.java
@@ -0,0 +1,8 @@
+package programowanieOb.figury;
+
+public class Glowna {
+    public static void main(String[] args) {
+        Prostakat prostakat = new Prostakat(30);
+        System.out.println(String.format("Parametry Twojego trojkata to : ") + prostakat);
+    }
+}
